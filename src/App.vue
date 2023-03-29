950b7da332a9fd4b3b3eb6d729a09769cb3d5563
Jerry Seyi <jerryseyi68@gmail.com>
2023-03-29 23:51:23 +0100

    introducing setup function

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default {
+      setup() {
+          const name = 'John Doe';
+
+          return {name};
+      }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>{{ name }}</div>
 </template>
 
 <style scoped>
